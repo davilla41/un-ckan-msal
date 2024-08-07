4b69da74b6cc27d5c5f09b024f7606b677e340cf
Admin Server User <david.villanueva@un.org>
2024-08-07 16:08:16 +0000

    Stop tracking ckanext/msal/msal_config.py

--- ckanext/msal/msal_config.py
+++
@@ -1,9 +0,0 @@
-# MSAL configurations.
-AUTHORITY = "https://login.microsoftonline.com/organizations"
-CLIENT_ID = "your_client_id"
-SCOPE = ["https://graph.microsoft.com/.default"]
-REDIRECT_URI = "http://localhost:5000/getAToken"
-CLIENT_SECRET = "yoursecret"
-
-# Plugin specific configurations.
-EMAIL_DOMAINS = ["google.com"]
