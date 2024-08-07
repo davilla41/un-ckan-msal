1ca72f0d0c804418945aacdc44b596133ddaa87d
Admin Server User <david.villanueva@un.org>
2024-08-07 15:53:01 +0000

    updating plugin.py file

--- ckanext/msal/plugin.py
+++ ckanext/msal/plugin.py
@@ -1,201 +1,146 @@
-# CKAN
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import logging
 import ckan.model as model
-from ckan.common import config, request, g
-from flask import Blueprint, render_template, render_template_string
+from ckan.common import config, request, g, session
+from flask import current_app as app, Blueprint, redirect, abort, url_for
 import ckan.lib.helpers as h
-
-# MSAL
 import msal
-
-# Plugin
 from . import msal_config
 
+
 log = logging.getLogger(__name__)
+
 application = msal.ConfidentialClientApplication(
-        msal_config.CLIENT_ID,
-        authority=msal_config.AUTHORITY,
-        client_credential=msal_config.CLIENT_SECRET
-    )
-
-
-def _validate_email_domains(user):
-    '''Validate user's email domain is allowed.
-    Potential for guest user to be added to Azure AD that
-    we don't want to be accepted in CKAN, even if added there.
-    '''
-    try:
-        domain = user.split('@')[1].lower()
-        if not domain in msal_config.EMAIL_DOMAINS:
-            raise Exception(user)
-    except Exception as e:
-        log.error('Exception raised. Improper email domain. {}'
-                  .format(repr(e)))
-        return False
-    return True
-
-
-def _validate_user_exists_in_ckan(user, user_name):
-    '''Validate the user is registered and active in CKAN.
-    Return boolean.
-    Checks if user exists based on username.
-    Checks if user is active.
-    Chekcs if user email is a complete match (just username may provide false
-    match to differnet domain).
-    Check state, state: deleted can still login but gets a blank page because
-    CKAN is handling authorization later as well.
-    '''
-    try:
-        user_obj = model.User.get(user_name)
-        if (user_obj and
-                user_obj.state == 'active' and
-                user_obj.email.lower() == user.lower()):
-            return True
-        else:
-            raise Exception(user_name)
-    except (toolkit.ObjectNotFound, Exception) as e:
-        log.error('Exception raised. Invalid user. {}'
-                  .format(repr(e)))
-        return False
-
+    msal_config.CLIENT_ID,
+    authority=msal_config.AUTHORITY,
+    client_credential=msal_config.CLIENT_SECRET
+)
 
 def msal_login():
-    '''Make call to authorization_url to authenticate user and get
-    authorization code.
-    '''
+    '''Make call to authorization_url to authenticate user and get authorization code.'''
+    log.debug('Starting MSAL login process')
     authorization_url = application.get_authorization_request_url(
-            msal_config.SCOPE,
-            redirect_uri=msal_config.REDIRECT_URI,
-            #prompt="login"
-        )
-
-    resp = toolkit.h.redirect_to(authorization_url)
+        msal_config.SCOPE,
+        redirect_uri=msal_config.REDIRECT_URI
+    )
     log.error(authorization_url)
-
-    return resp
-
+    log.debug('MSAL login initiated')
+    return redirect(authorization_url)
 
 def get_a_token():
-    '''Handle Azure AD callback.
-    Get authorization code from Azure AD response. Use code to get
-    token.
-    Returns response to dashboard if logged in or aborts with 403.
-    '''
+    '''Handle Azure AD callback.'''
     try:
+        log.debug("Starting get_a_token function")
         code = request.args['code']
+        log.debug(f"Authorization code received: {code}")
+        
+        result = application.acquire_token_by_authorization_code(
+            code,
+            scopes=msal_config.SCOPE,
+            redirect_uri=msal_config.REDIRECT_URI
+        )
+        log.debug(f"Token acquisition result: {result}")
 
-        result = application.acquire_token_by_authorization_code(code,
-                scopes=msal_config.SCOPE,
-                redirect_uri=msal_config.REDIRECT_URI
-            )
+        user = result.get("id_token_claims", {}).get("preferred_username")  # email
+        log.debug(f"User email from token: {user}")
+        
+        user_name = user.lower().replace('.', '_').split('@')[0].strip()  # ckan's username
+        log.debug(f"CKAN username: {user_name}")
 
-        user = result.get("id_token_claims", {}).get("preferred_username") # email
-        user_name = user.lower().replace('.', '_').split('@')[0].strip() # ckan'd username
-        # as of CKAN 2.9.6, user model is called by id not name, so need
-        # to define user_id
-        user_id = model.User.get(user_name).id
-        # CKAN 2.9.6 expects a serial_counter to be passed into environ
-        # Hard-code counter to '1' here
-        user_id = '{},{}'.format( user_id, 1)
+        user_obj = model.User.get(user_name)
+        log.debug(f"User object from CKAN: {user_obj}")
 
-        # Validate user info.
-        if not _validate_email_domains(user):
-            raise Exception(user)
-        if not _validate_user_exists_in_ckan(user, user_name):
-            raise Exception(user)
+        if not user_obj:
+            # Create a new user in CKAN
+            user_obj = model.User(name=user_name, email=user.lower(), password='default_password')
+            user_obj.save()
+            log.info(f"Created new user in CKAN: {user_name}")
+        else:
+            # Activate the user if not already active
+            if user_obj.state != 'active':
+                user_obj.state = 'active'
+                user_obj.save()
+                log.info(f"Activated user: {user_name}")
 
-        # Note: If developing locally make sure the site_url is set to http://localhost
-        #       and not 127.0.0.1 otherwise it will log the user out.
-        resp = toolkit.h.redirect_to('/dashboard')
+        log.debug(f"User {user} retrieved with username {user_name}")
 
-        # Set the repoze.who cookie to match a given user_id
-        if u'repoze.who.plugins' in request.environ:
-            rememberer = request.environ[u'repoze.who.plugins'][u'friendlyform']
-            # pass user_id and not user_name to identity to be compatible with
-            # breaking changes introduced in ckan 2.9.6
-            identity = {u'repoze.who.userid': user_id}
-            resp.headers.extend(rememberer.remember(request.environ, identity))
+        user_id = user_obj.id
+        log.debug(f"User ID: {user_id}")        
+
+        # Set session for the user        
+        session["_user_id"] = user_name
+        session.save()
+        log.debug(f"Session Object: {session}")
+        log.debug(f"Session set for user: {user_name}")
+        
+        
+        # Redirect to dataset
+        site_url = app.config.get('ckan.site_url')
+        resp = redirect(f'{site_url}/dashboard/datasets')
+        log.info(f"Redirecting to {site_url}/dashboard/datasets") 
+
+        log.debug("Redirecting to dataset")
+        return resp
+    except ValueError as e:
+        log.error('ValueError: {}'.format(repr(e)))
+        return toolkit.abort(403, 'Not authorized.')
     except Exception as e:
-        log.error('Exception raised. Unable to authenticate user. {}'
-                  .format(repr(e)))
-        toolkit.abort(403, 'Not authorized.')
-
-    return resp
+        log.error('Exception raised. Unable to authenticate user: {}'.format(repr(e)))
+        return toolkit.abort(403, 'Not authorized.')
 
 def _get_repoze_handler(handler_name):
-    u'''Returns the URL that repoze.who will respond to and perform a
-    login or logout.'''
-    return getattr(request.environ[u'repoze.who.plugins'][u'friendlyform'],
-                   handler_name)
-
-def override_logged_out():
-    '''Override the logged_out() function to call Microsoft account logout.
-    The IAuthenticator logout() did not seem to properly follow redirect 
-    (I think this is because it is an interactive page it goes to or because 
-    this is flask and there ins't a global response object like in pylons) and
-    continued on with the standard logout() logic.
-    Also, doing it at logged_out() vs logged_out_page() to prevent "came_from" param going to a
-    different page and stopping the logout below.
-    '''
-    h.flash_success('You are now logged out.')
-    return toolkit.h.redirect_to('https://login.microsoftonline.com/organizations/oauth2/v2.0/logout?post_logout_redirect_uri={}/user/login'.format(config.get('ckan.site_url')))
-
-
+    '''Returns the URL that repoze.who will respond to and perform a login or logout.'''
+    return getattr(request.environ['repoze.who.plugins']['friendlyform'], handler_name)
 
 class MsalPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IAuthenticator)
 
-
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'msal')
 
-
     # IAuthenticator
-
     def login(self):
         pass
 
     def identify(self):
         pass
 
-    def abort(self, status_code, detail, headers, comment):
-        pass
+    def abort(self, status_code, detail, headers=None, comment=None):
+        if headers is None:
+            headers = {}
+        if detail is None:
+            detail = ""
+        if comment is None:
+            comment = ""
+        return status_code, detail, headers, comment
 
     def logout(self):
-        log.error('logout')
-        url = toolkit.h.url_for(u'user.logged_out_page')
-        log.error(url)
-        log.error(_get_repoze_handler(u'logout_handler_path'))
-        redirect = _get_repoze_handler(u'logout_handler_path') + u'?came_from=' + url
-        log.error(redirect)
-        log.error(msal_config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=https://test.data.ontario.ca" + redirect)
-        #toolkit.h.redirect_to(msal_config.AUTHORITY + "/oauth2/v2.0/logout" + "?post_logout_redirect_uri=https://test.data.ontario.ca/user/_logout")
-        return h.redirect_to('/')
-        #toolkit.h.redirect_to('/dataset')
-        #log.error(resp.__dict__)
-        #return resp
+      try:
+        # Asumiendo que quieres redirigir al usuario a la página de logout de MS
+        session.clear()
+        msad_url = msal_config.AUTHORITY
+        site_url = app.config.get('ckan.site_url')
+        logout_url = f"{msad_url}/oauth2/v2.0/logout?post_logout_redirect_uri={site_url}/user/login"
+        log.info(f"Redirecting to logout URL: {logout_url}")
+        return redirect(logout_url)
+      except Exception as e:
+        log.error(f"Error during logout: {e}")
+        return toolkit.abort(500, 'Internal Server Error')
 
     # IBlueprint
-
     def get_blueprint(self):
         blueprint = Blueprint(self.name, self.__module__)
         rules = [
             ('/msal/login', 'msal_login', msal_login),
-            ('/getAToken', 'get_a_token', get_a_token),
-            ('/user/logged_out', 'logged_out', override_logged_out)
+            ('/getAToken', 'get_a_token', get_a_token)
+            # ('/user/logged_out', 'logged_out', override_logged_out)
         ]
-
         for rule in rules:
             blueprint.add_url_rule(*rule)
-
         return blueprint
-
